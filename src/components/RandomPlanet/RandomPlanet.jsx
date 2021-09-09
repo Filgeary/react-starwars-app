64f2f0dc23e08fbf5b19b7dd4c16c54d7809a788
Georgiy Filippov <g.filippov777@gmail.com>
2021-09-09 21:28:46 +0500

    add data rendering to `RandomPlanet`

--- src/components/RandomPlanet/RandomPlanet.jsx
+++ src/components/RandomPlanet/RandomPlanet.jsx
@@ -1,30 +1,58 @@
 import React, { Component } from 'react'
+import SwapiService from '../../services/SwapiService'
 import './RandomPlanet.css'
 
+const swapiService = new SwapiService()
+
 class RandomPlanet extends Component {
+  constructor() {
+    super()
+    this.updatePlanet()
+  }
+
+  state = {
+    planet: {},
+  }
+
+  handlePlanetLoaded = data => {
+    this.setState({ planet: data })
+  }
+
+  updatePlanet = () => {
+    // TODO: replace to Math random
+    const idx = '7'
+
+    swapiService
+      .getPlanet(idx)
+      .then(this.handlePlanetLoaded)
+      .catch(err => console.error(err))
+  }
+
   render() {
+    const { id, name, population, rotationPeriod, diameter } = this.state.planet
+
     return (
       <div className="random-planet jumbotron rounded">
         <img
           className="planet-image"
-          src="https://starwars-visualguide.com/assets/img/planets/5.jpg"
-          alt="planet"
+          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+          alt={name}
         />
         <div>
-          <h3 className="random-planet-title">Planet Name</h3>
+          <h3 className="random-planet-title">{name}</h3>
 
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <span className="term">Population</span>
-              <span>123124</span>
+              <span>{population}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Rotation Period</span>
-              <span>43</span>
+              <span>{rotationPeriod}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Diameter</span>
-              <span>100</span>
+              <span>{diameter}</span>
             </li>
           </ul>
         </div>
