8f9e398eb9d2928350ecbc61191d3038dcf26547
Georgiy Filippov <g.filippov777@gmail.com>
2021-09-11 01:47:36 +0500

    add data rendering to `ItemList`

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Header from '../Header/Header'
 import ItemList from '../ItemList/ItemList'
 import PersonDetails from '../PersonDetails/PersonDetails'
 import RandomPlanet from '../RandomPlanet/RandomPlanet'
 import './App.css'
 
-const App = () => {
-  return (
-    <div className="container-lg">
-      <Header />
-      <RandomPlanet />
+class App extends Component {
+  state = {
+    itemSelected: '',
+  }
 
-      <div className="row mb-2">
-        <div className="col-md-6">
-          <ItemList />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails />
+  handleItemSelected = id => {
+    this.setState({ itemSelected: id })
+    console.log(id)
+  }
+
+  render() {
+    return (
+      <div className="container-lg">
+        <Header />
+        <RandomPlanet />
+
+        <div className="row mb-2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.handleItemSelected} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails />
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App
