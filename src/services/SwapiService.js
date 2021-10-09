c48e5336c22fe0a6703994d4434a0893be065269
Georgiy Filippov <g.filippov777@gmail.com>
2021-10-09 21:36:56 +0500

    fix - formatting

--- src/services/SwapiService.js
+++ src/services/SwapiService.js
@@ -13,46 +13,39 @@ export default class SwapiService {
         `Failed to fetch ${this.#baseUrl + endpoint}, status: ${res.status}`,
       )
     }
-
     return await res.json()
   }
 
   // people
   getAllPeople = async () => {
     const res = await this.#getResource('/people')
-
     return res.results.map(adapter.adaptPerson)
   }
 
   getPerson = async id => {
     const res = await this.#getResource(`/people/${id}`)
-
     return adapter.adaptPerson(res)
   }
 
   // planets
   getAllPlanets = async () => {
     const res = await this.#getResource('/planets')
-
     return res.results.map(adapter.adaptPlanet)
   }
 
   getPlanet = async id => {
     const res = await this.#getResource(`/planets/${id}`)
-
     return adapter.adaptPlanet(res)
   }
 
   // starships
   getAllStarships = async () => {
     const res = await this.#getResource('/starships')
-
     return res.results.map(adapter.adaptStarship)
   }
 
   getStarship = async id => {
     const res = await this.#getResource(`/starships/${id}`)
-
     return adapter.adaptStarship(res)
   }
 }
