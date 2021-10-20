c60e6291ec19af9b1da98b29006cf7bce1ca0066
Georgiy Filippov <g.filippov777@gmail.com>
2021-10-20 05:45:08 +0500

    refactor - add hooks to `RandomPlanet`

--- src/components/RandomPlanet/RandomPlanet.jsx
+++ src/components/RandomPlanet/RandomPlanet.jsx
@@ -1,103 +1,88 @@
-import React, { Component } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import SwapiService from '../../services/SwapiService'
 import { randomInt } from '../../utils/utils'
 import ErrorMessage from '../ErrorMessage/ErrorMessage'
 import Spinner from '../Spinner/Spinner'
 import './RandomPlanet.css'
 
-const { getPlanet } = new SwapiService()
-let timerId = 0
+const { getPlanet, getPlanetImageUrl } = new SwapiService()
 
-class RandomPlanet extends Component {
-  state = {
-    planet: {},
-    isLoading: true,
-    isError: false,
+const RandomPlanet = () => {
+  const [planet, setPlanet] = useState({})
+  const [imageUrl, setImageUrl] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+
+  const handlePlanetLoaded = data => {
+    setPlanet(data)
+    setImageUrl(getPlanetImageUrl(data))
+    setIsLoading(false)
+    setIsError(false)
   }
 
-  handlePlanetLoaded = data => {
-    this.setState({ planet: data, isLoading: false, isError: false })
-  }
-
-  handleError = err => {
-    this.setState({ isError: true, isLoading: false })
+  const handleError = err => {
+    setIsError(true)
+    setIsLoading(false)
     console.error(err)
   }
 
-  updatePlanet = () => {
-    this.setState({ isLoading: true })
-
+  const updatePlanet = useCallback(() => {
+    setIsLoading(true)
     const idx = String(randomInt(2, 19))
 
     // prettier-ignore
     getPlanet(idx)
-      .then(this.handlePlanetLoaded)
-      .catch(this.handleError)
-  }
+      .then(handlePlanetLoaded)
+      .catch(handleError)
+  }, [])
 
-  componentDidMount() {
-    this.updatePlanet()
+  useEffect(() => {
+    updatePlanet()
 
-    timerId = setInterval(() => {
-      this.updatePlanet()
+    let timerId = setInterval(() => {
+      updatePlanet()
     }, 15000)
-  }
 
-  componentWillUnmount() {
-    clearInterval(timerId)
-  }
+    return () => clearInterval(timerId)
+  }, [updatePlanet])
 
-  render() {
-    const { planet, isLoading, isError } = this.state
-
-    return (
-      <div className="random-planet jumbotron rounded col-md-8 offset-2">
-        {isLoading && Object.keys(planet).length === 0 ? <Spinner /> : null}
-        {isError ? <ErrorMessage /> : null}
-
-        {!isError && Object.keys(planet).length > 0 ? (
-          <RandomPlanetView planet={planet} />
-        ) : null}
-      </div>
-    )
-  }
-}
-
-const RandomPlanetView = ({ planet }) => {
   const { id, name, population, rotationPeriod, diameter } = planet
 
   return (
-    <React.Fragment>
-      <div className="random-planet-image-wrapper">
-        <img
-          className="random-planet-image"
-          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-          alt={name}
-        />
-      </div>
+    <div className="random-planet jumbotron rounded col-md-8 offset-2">
+      {isLoading && Object.keys(planet).length === 0 ? <Spinner /> : null}
+      {isError ? <ErrorMessage /> : null}
 
-      <div className="random-planet-content">
-        <h3 className="random-planet-title">
-          {name}&nbsp;
-          <span className="random-planet-id">#{id}</span>
-        </h3>
+      {!isError && Object.keys(planet).length > 0 ? (
+        <>
+          <div className="random-planet-image-wrapper">
+            <img className="random-planet-image" src={imageUrl} alt={name} />
+          </div>
 
-        <ul className="list-group list-group-flush">
-          <li className="random-planet-list-item list-group-item">
-            <span className="term">Population</span>
-            <span>{population}</span>
-          </li>
-          <li className="random-planet-list-item list-group-item">
-            <span className="term">Rotation Period</span>
-            <span>{rotationPeriod}</span>
-          </li>
-          <li className="random-planet-list-item list-group-item">
-            <span className="term">Diameter</span>
-            <span>{diameter}</span>
-          </li>
-        </ul>
-      </div>
-    </React.Fragment>
+          <div className="random-planet-content">
+            <h3 className="random-planet-title">
+              {name}&nbsp;
+              <span className="random-planet-id">#{id}</span>
+            </h3>
+
+            <ul className="list-group list-group-flush">
+              <li className="random-planet-list-item list-group-item">
+                <span className="term">Population</span>
+                <span>{population}</span>
+              </li>
+              <li className="random-planet-list-item list-group-item">
+                <span className="term">Rotation Period</span>
+                <span>{rotationPeriod}</span>
+              </li>
+              <li className="random-planet-list-item list-group-item">
+                <span className="term">Diameter</span>
+                <span>{diameter}</span>
+              </li>
+            </ul>
+          </div>
+        </>
+      ) : null}
+    </div>
   )
 }
 
