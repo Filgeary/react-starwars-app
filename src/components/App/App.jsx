49eaca49f372a4d85c7d8710ee0e099fa00f3962
Georgiy Filippov <g.filippov777@gmail.com>
2021-10-25 10:46:55 +0500

    add `Switch` & `NotFoundPage` to Routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage'
 import PeoplePage from '../../pages/PeoplePage/PeoplePage'
 import PlanetsPage from '../../pages/PlanetsPage/PlanetsPage'
 import StarshipsPage from '../../pages/StarshipsPage/StarshipsPage'
@@ -15,15 +16,19 @@ const App = () => {
       <div className="container-lg">
         <Header />
 
-        <Route path="/" exact>
-          <WelcomeMessage />
-          <RandomPlanet />
-          <WelcomeStory />
-        </Route>
+        <Switch>
+          <Route path="/" exact>
+            <WelcomeMessage />
+            <RandomPlanet />
+            <WelcomeStory />
+          </Route>
 
-        <Route path="/people" component={PeoplePage} />
-        <Route path="/planets" component={PlanetsPage} />
-        <Route path="/starships" component={StarshipsPage} />
+          <Route path="/people" component={PeoplePage} />
+          <Route path="/planets" component={PlanetsPage} />
+          <Route path="/starships" component={StarshipsPage} />
+
+          <Route component={NotFoundPage} />
+        </Switch>
       </div>
     </BrowserRouter>
   )
