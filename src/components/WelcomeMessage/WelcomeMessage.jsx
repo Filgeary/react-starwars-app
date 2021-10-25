0ca5e1e8f9d55f20f4876bdd6d2d6a8a87b1979c
Georgiy Filippov <g.filippov777@gmail.com>
2021-10-25 12:32:36 +0500

    fix styles

--- src/components/WelcomeMessage/WelcomeMessage.jsx
+++ src/components/WelcomeMessage/WelcomeMessage.jsx
@@ -4,9 +4,9 @@ import './WelcomeMessage.css'
 const WelcomeMessage = () => {
   return (
     <div className="welcome-message">
-      <h3 className="welcome-message__title">
+      <p className="welcome-message__title">
         A long time ago in a galaxy far, far away ...
-      </h3>
+      </p>
     </div>
   )
 }
